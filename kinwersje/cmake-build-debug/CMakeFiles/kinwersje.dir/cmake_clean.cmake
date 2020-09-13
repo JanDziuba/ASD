530d369fccc7cf31e3020300c816cbe79b73cb90
JanDziuba <71147908+JanDziuba@users.noreply.github.com>
2020-09-13 22:25:19 +0200

    Add files via upload

---
+++ kinwersje/cmake-build-debug/CMakeFiles/kinwersje.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/kinwersje.dir/main.cpp.o"
+  "kinwersje"
+  "kinwersje.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/kinwersje.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
